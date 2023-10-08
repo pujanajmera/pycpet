7cad57616d5f6d217a3ec8a0fe4aba52796c64ba
pujanajmera <ajmerapujan@gmail.com>
2023-10-08 04:13:00 -0700

    Create fastmath.py

---
+++ CPET/utils/fastmath.py
@@ -0,0 +1,31 @@
+import numpy as np
+from numba import jit
+
+def power(a,b):
+    """
+    Computes binary exponentiation
+    Takes
+        a(float or array) - a number or array of shape (N,) or (N,1)
+        b(float) - a real positive number
+    Returns
+        out_(float or array) - element-wise exponentiation of a by b, a number or array of shape (N,) or (N,1)
+    """
+    if b==0:
+        return 1
+    out_ = power(a,b//2)
+    out_ = out_ * out_
+    if b%2 !=0 :
+        out_ = out_ * a
+    return out_
+
+@jit(nopython=True)
+def nb_subtract(a,b):
+    """
+    Computes subtraction
+    Takes
+        a(array or array-like)
+        b(array or array-like)
+    Returns
+        np.subtract(a,b) - difference between a and b - jitted!
+    """
+    return np.subtract(a,b)
