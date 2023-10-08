5019cff4b87b12afa89d0002e763357fb9236260
pujanajmera <ajmerapujan@gmail.com>
2023-10-08 04:14:06 -0700

    Update compute_topology.py

--- CPET/source/compute_topology.py
+++ CPET/source/compute_topology.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 from numba import jit
 from CPET.utils.fastmath import power, nb_subtract
+from CPET.utils.parser import parse_pqr
 
 def calculate_electric_field(x_0, x, Q):
     """
@@ -69,33 +70,6 @@ def compute_curv_and_dist(x_init, x_init_plus, x_init_plus_plus, x_0, x_0_plus, 
     curv_mean = (curv_init+curv_final)/2
     dist = np.linalg.norm(x_init-x_0)
     return [dist, curv_mean]
-
-def parse_pqr(path_to_pqr):
-    """
-    Parses pqr file to obtain charges and positions of charges (beta, removes charges that are 0)
-    Takes
-        path_to_pqr(str) - path to pqr file
-    Returns
-        np.array(x)(array) - coordinates of charges of shape (N,3)
-        np.array(Q).reshape(-1,1) - magnitude and sign of charges of shape (N,1)
-    """
-    x = []
-    Q = []
-    with open(path_to_pqr) as pqr_file:
-        lines = pqr_file.readlines()
-    for line in lines:
-        if line.startswith("ATOM") or line.startswith("HETATM"):
-            coords = [line[31:39].strip(),line[40:48].strip(),line[49:57].strip()]
-
-            charge = line[58:63].strip()
-            try:
-                tempq = float(charge)
-                temp = [float(_) for _ in coords]
-            except:
-                print(f"Charge or coordinates is not a useable number. Check pqr file formatting for the following line: {line}")
-            x.append(temp)
-            Q.append(tempq)
-    return np.array(x), np.array(Q).reshape(-1,1)
 
 def initialize_box_points(center,
                           x,
