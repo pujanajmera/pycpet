9505d631d436f3d4d354b28fe351947c6af12920
pujanajmera <ajmerapujan@gmail.com>
2023-10-10 00:52:42 -0700

    Create writeoutput.py

---
+++ CPET/utils/writeoutput.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def write_field_to_file(grid_points, field_points, filename):
+    """
+    Write grid points and field points to a files
+    Takes:
+        grid_points(array): 3D grid points (shape: (M,M,M 3))
+        field_points(array): Corresponding field values (shape: (M,M,M,3))
+        filename(str): Name of the file to write
+    """
+    # Flatten the grid and field points arrays and stack them along the last axis
+    data = np.column_stack((grid_points.reshape(-1, 3), field_points.reshape(-1, 3)))
+    # Create a format string for writing each line of the file
+    format_str = ' '.join(['%f'] * 6)
+    # Open the file for writing
+    with open(filename, 'w') as f:
+        # Write 7 lines of hashtags
+        for _ in range(7):
+            f.write('#\n')
+        # Write the data to the file
+        np.savetxt(f, data, fmt=format_str)
