c1f43a5f0dbb9c6623e70968f04d87b665b61316
Santiago Vargas <santiagovargas921@gmail.com>
2023-11-08 13:59:59 -0500

    more linting

--- CPET/source/compute_field_volume.py
+++ CPET/source/compute_field_volume.py
@@ -5,6 +5,7 @@ from CPET.utils.parser import parse_pqr
 from CPET.utils.calculator import compute_field_on_grid
 from CPET.utils.writeoutput import write_field_to_file
 
+
 def initialize_grid(center, x, y, density, dimensions, offset):
     # Convert lists to numpy arrays
     x = x - center  # Translate to origin
@@ -20,20 +21,23 @@ def initialize_grid(center, x, y, density, dimensions, offset):
     y_unit = y_unit / np.linalg.norm(y_unit)
     transformation_matrix = np.column_stack([x_unit, y_unit, z_unit])
     # Create arrays of coordinates along each axis
-    x_coords = np.linspace(-dimensions[0], dimensions[0], 2*density[0]+1)
-    y_coords = np.linspace(-dimensions[1], dimensions[1], 2*density[1]+1)
-    z_coords = np.linspace(-dimensions[2], dimensions[2], 2*density[2]+1)
+    x_coords = np.linspace(-dimensions[0], dimensions[0], 2 * density[0] + 1)
+    y_coords = np.linspace(-dimensions[1], dimensions[1], 2 * density[1] + 1)
+    z_coords = np.linspace(-dimensions[2], dimensions[2], 2 * density[2] + 1)
     # Create a meshgrid of coordinates
     x_mesh, y_mesh, z_mesh = np.meshgrid(x_coords, y_coords, z_coords)
     # Stack the meshgrid coordinates into a single array of shape (density[0], density[1], density[2], 3)
     local_coords = np.stack([x_mesh, y_mesh, z_mesh], axis=-1)
     return local_coords, transformation_matrix
 
-parser = argparse.ArgumentParser(description='This script computes a field in a box volume with given density')
-parser.add_argument('-i', help='Input file name')
-parser.add_argument('-o', help='Output file name')
+
+parser = argparse.ArgumentParser(
+    description="This script computes a field in a box volume with given density"
+)
+parser.add_argument("-i", help="Input file name")
+parser.add_argument("-o", help="Output file name")
 args = parser.parse_args()
-with open(args.i, 'r') as file:
+with open(args.i, "r") as file:
     input_data = json.load(file)
 center = np.array(input_data["center"])
 x = np.array(input_data["x"])
@@ -42,8 +46,10 @@ dimensions = np.array(input_data["dimensions"])
 density = np.array(input_data["density"])
 offset = input_data["offset"]
 path_to_pqr = input_data["pqr_file"]
-grid_points, transformation_matrix = initialize_grid(center, x, y, density, dimensions, offset)
+grid_points, transformation_matrix = initialize_grid(
+    center, x, y, density, dimensions, offset
+)
 x, Q = parse_pqr(path_to_pqr)
-x = (x-center)@transformation_matrix
+x = (x - center) @ transformation_matrix
 field_points = compute_field_on_grid(grid_points, x, Q)
 write_field_to_file(grid_points, field_points, args.o)
