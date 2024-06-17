175c3c8c44c217b880ee1e3dca77789e16388220
Santiago Vargas <santiagovargas921@gmail.com>
2024-06-17 12:35:40 -0400

    update test for pytest

--- tests/test_e_field_calcs.py
+++ tests/test_e_field_calcs.py
@@ -22,104 +22,110 @@ from CPET.utils.calculator import (
     )
 
 
-class test_efield_calcs:
-    def __init__(self):
-        self.test_file = "./test_files/test_large.pdb"
-        self.options = {
-            "dtype": "float32", 
-            "center": {
-                "method": "first", 
-                "atoms": {
-                    "CD": 2
-                }
-            }, 
-            "x":[1, 0, 0],
-            "y": [0, 1, 0],
-            "filter_radius": 10.0,
-            "initializer": "uniform", 
-            "dimensions":  [
-                    2.0,
-                    2.0,
-                    2.0
-            ], 
-            "filter_in_box": True, 
-            "CPET_method": "woohoo",
-            "batch_size": 100
 
-        }
-        # use existing machinery to generate mesh of starting points
-        self.topo = calculator(self.options, path_to_pdb = self.test_file)
-        self.gather_reference_from_point_utility() # this is ground 
-        self.gather_reference_simplest_implementation() # this is ground 
-        self.gather_reference_from_utility() # this yields inf values
+
+def gather_reference_from_utility(topo):
+    #field = self.topo.compute_box()
+    field = compute_field_on_grid(
+        grid_coords=topo.random_start_points, 
+        x=topo.x, 
+        Q=topo.Q
+    )
+    field = np.round(field, 3)
+    print("efield shape: {}".format(field.shape))
+    print(field[1000:1001])
+    # round field to 3 decimal places
+    print("inf values: {}".format(np.isinf(field).any()))
+    
+    utility_field = field
+    return utility_field
+
+
+def gather_reference_from_point_utility(topo):
+    field_list = []
+    for i in topo.random_start_points:
+        field = calculate_field_at_point(
+            Q=topo.Q, 
+            x=topo.x, 
+            x_0=i
+        )
+        field_list.append(field)
+
+    field = np.array(field_list)
+    # convert convert self.topo.random_start_points(N, 3) and fields(N, 3) to (N, 6)
+    field_formatted = np.hstack((topo.random_start_points, field))
+    field_formatted = np.round(field_formatted, 3)
+    print("efield shape: {}".format(field_formatted.shape))
+    print(field_formatted[1000:1001])
+    print("inf values: {}".format(np.isinf(field_formatted).any()))
+    point_utility_field = field_formatted
+    return point_utility_field
+    
+
+def gather_reference_simplest_implementation(topo): 
+    field_list = []
+    for i in topo.random_start_points:
+        field = calculate_electric_field_base(
+            Q=topo.Q, 
+            x=topo.x, 
+            x_0=i
+        )
+        field_list.append(field)
+
+    field = np.array(field_list)
+    # convert convert self.topo.random_start_points(N, 3) and fields(N, 3) to (N, 6)
+    field_formatted = np.hstack((topo.random_start_points, field))
+    field_formatted = np.round(field_formatted, 3)
+    print("efield shape: {}".format(field_formatted.shape))
+    print("inf values: {}".format(np.isinf(field_formatted).any()))
+    print(field_formatted[1000:1001])
+    # round field to 3 decimal places
+    
+    reference_field = field_formatted
+    return reference_field
+        
+
+class TestEField:
+    test_file = "./test_files/test_large.pdb"
+    options = {
+        "dtype": "float32", 
+        "center": {
+            "method": "first", 
+            "atoms": {
+                "CD": 2
+            }
+        }, 
+        "x":[1, 0, 0],
+        "y": [0, 1, 0],
+        "filter_radius": 10.0,
+        "initializer": "uniform", 
+        "dimensions":  [
+                2.0,
+                2.0,
+                2.0
+        ], 
+        "filter_in_box": True, 
+        "CPET_method": "woohoo",
+        "batch_size": 100
+
+    }
+    # use existing machinery to generate mesh of starting points
+    topo = calculator(options, path_to_pdb = test_file)
+    point_utility_field = gather_reference_from_point_utility(topo) # this is ground 
+    reference_field = gather_reference_simplest_implementation(topo) # this is ground 
+    #gather_reference_from_utility(topo) # this yields inf values
 
     # ground truth can be our compute_field_on_grid or the simplest cpu implementation we have
 
-    def gather_reference_from_utility(self):
-        #field = self.topo.compute_box()
-        field = compute_field_on_grid(
-            grid_coords=self.topo.random_start_points, 
-            x=self.topo.x, 
-            Q=self.topo.Q
-        )
-        field = np.round(field, 3)
-        print("efield shape: {}".format(field.shape))
-        print(field[1000:1001])
-        # round field to 3 decimal places
-        print("inf values: {}".format(np.isinf(field).any()))
-        
-        self.utility_field = field
 
-
-    def gather_reference_from_point_utility(self):
-        field_list = []
-        for i in self.topo.random_start_points:
-            field = calculate_field_at_point(
-                Q=self.topo.Q, 
-                x=self.topo.x, 
-                x_0=i
-            )
-            field_list.append(field)
-
-        field = np.array(field_list)
-        # convert convert self.topo.random_start_points(N, 3) and fields(N, 3) to (N, 6)
-        field_formatted = np.hstack((self.topo.random_start_points, field))
-        field_formatted = np.round(field_formatted, 3)
-        print("efield shape: {}".format(field_formatted.shape))
-        print(field_formatted[1000:1001])
-        print("inf values: {}".format(np.isinf(field_formatted).any()))
-        self.point_utility_field = field_formatted
-
-        
-    def gather_reference_simplest_implementation(self): 
-        field_list = []
-        for i in self.topo.random_start_points:
-            field = calculate_electric_field_base(
-                Q=self.topo.Q, 
-                x=self.topo.x, 
-                x_0=i
-            )
-            field_list.append(field)
-
-        field = np.array(field_list)
-        # convert convert self.topo.random_start_points(N, 3) and fields(N, 3) to (N, 6)
-        field_formatted = np.hstack((self.topo.random_start_points, field))
-        field_formatted = np.round(field_formatted, 3)
-        print("efield shape: {}".format(field_formatted.shape))
-        print("inf values: {}".format(np.isinf(field_formatted).any()))
-        print(field_formatted[1000:1001])
-        # round field to 3 decimal places
-        
-        self.reference_field = field_formatted
-        
     
-    def test_two_reference_fields(self):
-        np.testing.assert_allclose(self.point_utility_field, self.reference_field, rtol=1e-2, atol=1e-2)
-
-
     def field_equality(self, test_field):
         np.testing.assert_allclose(self.reference_field, test_field, rtol=1e-2, atol=1e-2)
         np.testing.assert_allclose(self.point_utility_field, test_field, rtol=1e-2, atol=1e-2)
+
+
+    def test_two_reference_fields(self):
+        np.testing.assert_allclose(self.point_utility_field, self.reference_field, rtol=1e-2, atol=1e-2)
 
 
     def test_calculators(self):
