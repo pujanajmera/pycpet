2c4b9dccbdbc316cb695ed154b20c867411fe0e9
Pujan Ajmera <pujanajmera1@g.ucla.edu>
2025-02-13 21:16:01 -0500

    Update conf

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,7 @@ author = 'Pujan Ajmera, Santiago Vargas, Matthew Hennefarth, Alexandrova Group'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
