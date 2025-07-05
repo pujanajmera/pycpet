6c9f339c73b5f0c14f5c661b5512e3a75a4c84c4
Pujan Ajmera <pujanajmera1@g.ucla.edu>
2025-07-05 06:28:08 -0400

    Update theme

--- docs/conf.py
+++ docs/conf.py
@@ -64,8 +64,9 @@ autodoc_mock_imports = [
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+# Use readthedocs theme
 #
-html_theme = "sphinx_book_theme"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
